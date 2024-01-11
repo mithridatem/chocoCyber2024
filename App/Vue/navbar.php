1ca371db2557769a63517aae84a14d83402e461d
mithridatem <mathieumithridate@adrar-formation.com>
2024-01-11 10:59:21 +0100

    menu connecté et déconnecté

--- App/Vue/navbar.php
+++ App/Vue/navbar.php
@@ -1,11 +1,14 @@
 <?php ob_start()?>
 <?php if(isset($_SESSION['connected'])):?>
 <ul>
-    <li><a href=./>Accueil</a></li>
+    <li><a href="/choco2024">Accueil</a></li>
+    <?=$_SESSION["nom"]."<br>"?>
+    <?=$_SESSION["prenom"]?>
+    <img src="<?=$_SESSION["image"]?>" alt="" class="profil">
 </ul>
 <?php else:?>
 <ul>
-    <li><a href=./>Accueil</a></li>
+    <li><a href="/choco2024">Accueil</a></li>
 </ul>
 <?php endif;?>
 <?php $navbar = ob_get_clean()?>
