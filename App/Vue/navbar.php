51299439ab8556c6b699003c5c6850ac9ff6cc4b
mithridatem <72612782+mithridatem@users.noreply.github.com>
2024-01-11 11:49:04 +0100

    Update navbar.php

--- App/Vue/navbar.php
+++ App/Vue/navbar.php
@@ -2,8 +2,8 @@
 <!-- Menu connecté -->
 <?php if(isset($_SESSION['connected'])):?>
 <ul>
-    <li><a href="/choco2024">Accueil</a></li>
-    <li><a href="/choco2024/utilisateur/deconnexion">deconnexion</a></li>
+    <li><a href="/chocoCyber2024">Accueil</a></li>
+    <li><a href="/chocoCyber2024/utilisateur/deconnexion">deconnexion</a></li>
     <?=$_SESSION["nom"]."<br>"?>
     <?=$_SESSION["prenom"]?>
     <img src="<?=$_SESSION["image"]?>" alt="" class="profil">
@@ -11,9 +11,9 @@
 <!-- Menu déconnecté -->
 <?php else:?>
 <ul>
-    <li><a href="/choco2024">Accueil</a></li>
-    <li><a href="/choco2024/utilisateur/add">S'inscrire</a></li>
-    <li><a href="/choco2024/utilisateur/connexion">Se connecter</a></li>
+    <li><a href="/chocoCyber2024">Accueil</a></li>
+    <li><a href="/chocoCyber2024/utilisateur/add">S'inscrire</a></li>
+    <li><a href="/chocoCyber2024/utilisateur/connexion">Se connecter</a></li>
 </ul>
 <?php endif;?>
 <?php $navbar = ob_get_clean()?>
